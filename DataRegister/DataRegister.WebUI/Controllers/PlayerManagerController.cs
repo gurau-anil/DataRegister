b0c68891114157ce0a8ad7bf613c593d49b4b2b9
Anil gurau <gurau.anil@gmail.com>
2020-08-14 23:40:23 +0545

    Delete and Edit

--- DataRegister/DataRegister.WebUI/Controllers/PlayerManagerController.cs
+++ DataRegister/DataRegister.WebUI/Controllers/PlayerManagerController.cs
@@ -45,5 +45,95 @@ namespace DataRegister.WebUI.Controllers
             }
             
         }
+
+        public ActionResult Edit(string Id)
+        {
+            if(Id==null)
+            {
+                return View("Error");
+            }
+            
+            else
+            {
+                player = repository.Find(Id);
+                if (player==null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View(player);
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Player player) //public ActionResult Edit(Player item,string Id)
+        {
+            //Player playerToEdit = repository.Find(Id);
+            //if (playerToEdit == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            //else
+            //{
+            //    if (!ModelState.IsValid)
+            //    {
+            //        return View(item);
+            //    }
+            //    playerToEdit.FirstName = item.FirstName;
+            //    playerToEdit.LastName = item.LastName;
+            //    playerToEdit.Phone = item.Phone;
+            //    playerToEdit.Email = item.Email;
+            //    playerToEdit.Image = item.Image;
+            //    repository.Update(item);
+
+            //    repository.Commit();
+            //    return RedirectToAction("Index");
+            //}
+            if(ModelState.IsValid)
+            {
+                repository.Update(player);
+                repository.Commit();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(player);
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            if(Id==null)
+            {
+                return View("Error");
+            }
+            player = repository.Find(Id);
+            if(player==null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(player);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            player = repository.Find(Id);
+            if (player == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                repository.Delete(Id);
+                repository.Commit();
+                return RedirectToAction("Index");
+            }
+
+        }
     }
 }
