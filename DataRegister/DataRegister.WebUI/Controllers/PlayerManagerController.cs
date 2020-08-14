cb9a2448de8cc71c471d873658619d5c0c304536
Anil gurau <gurau.anil@gmail.com>
2020-08-14 21:36:58 +0545

    Player manager controller and its views

---
+++ DataRegister/DataRegister.WebUI/Controllers/PlayerManagerController.cs
@@ -0,0 +1,49 @@
+﻿using DataRegister.Base;
+using DataRegister.Base.Models;
+using DataRegister.SQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataRegister.WebUI.Controllers
+{
+    public class PlayerManagerController : Controller
+    {
+        IRepository repository;
+        Player player;
+        IEnumerable<Player> players;
+        public PlayerManagerController()
+        {
+            
+            player = new Player();
+            repository = new SqlRepository();
+            players = repository.Collection();
+
+        }
+        public ActionResult Index()
+        {
+            return View(players);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            if(ModelState.IsValid)
+            {
+                repository.Insert(player);
+                repository.Commit();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(player);
+            }
+            
+        }
+    }
+}
